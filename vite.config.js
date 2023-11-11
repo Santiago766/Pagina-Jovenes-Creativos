ccc678087cd492fde275b5e1cc7ff65c4e78935d
Santiago Castaño <santiagocastano1992@gmail.com>
2023-11-11 09:05:32 -0500

    se cambio a hashe 2.0

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://santiago766.github.io/Pagina-Jovenes-Creativos/'
+  base: "https://santiago766.github.io/Pagina-Jovenes-Creativos/"
 })
