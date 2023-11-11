d56cf54d2bc0b25debcf612ab0c704498c72e5d8
Santiago Castaño <santiagocastano1992@gmail.com>
2023-11-11 09:28:21 -0500

    se cambio a hashe 3.0

--- src/view/routes/routes.jsx
+++ src/view/routes/routes.jsx
@@ -6,7 +6,7 @@ import { Registro } from '../Pages/Registro/Registro'
 import { VerTareas } from '../Pages/Ver-Tareas/VerTareas'
 import { AgregarTarea } from '../Pages/AgregarTarea/AgregarTarea'
 
-
+  
 
 
 export const router = createHashRouter([
